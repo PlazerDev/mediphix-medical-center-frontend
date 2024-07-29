ccac9058cffaca38a784ddee9143e375645292a3
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2024-07-30 04:16:44 +0530

    added tailwind css and antd

---
+++ src/routes/MedicalCenterStaffRoutes.tsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+function MedicalCenterStaffRoutes() {
+  return (
+    <div className="bg-red-400">
+      <Button type="primary">asdasd</Button>MedicalCenterStaffRoutes
+    </div>
+  );
+}
+
+export default MedicalCenterStaffRoutes;
