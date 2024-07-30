92aa5ecce223fe142367c2f7690abda5916cbd34
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2024-07-30 10:30:56 +0530

    Specify port in vite.config

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
     const asgardioConfig = {
-        signInRedirectURL: "http://localhost:5173",
-        signOutRedirectURL: "http://localhost:5173",
+        signInRedirectURL: "http://localhost:5175",
+        signOutRedirectURL: "http://localhost:5175",
         clientID: import.meta.env.VITE_PATIENT_ASGARDEO_CLIENT_ID,
         baseUrl: import.meta.env.VITE_PATIENT_ASGARDEO_BASE_URL,
         scope: ["openid", "email", "profile", "insert_appointment", "retrieve_own_patient_data"]
