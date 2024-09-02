ca41912b68dc8e38c8c4a69c043bec98a73cb9a7
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2024-09-03 05:06:09 +0530

    added breadcrumbs

--- src/components/mcs/MCSGreetingVerificationName.tsx
+++ src/components/mcs/MCSGreetingVerificationName.tsx
@@ -3,16 +3,15 @@ import companyLogo from "./../../assets/images/mcs/medical_center_logo.png";
 
 interface Props {
   medicalCenterName: string;
+  role: string;
 }
 
-function MCSGreetingVerificationName({ medicalCenterName }: Props) {
+function MCSGreetingVerificationName({ medicalCenterName, role }: Props) {
   return (
     <div className="bg-mediphix_card_background inline-flex items-center gap-4 px-4 py-2 rounded-lg">
       <img src={companyLogo} className="rounded-2xl w-12 h-12" />
       <div>
-        <p className="font-bold text-mediphix_accent">
-          Medical Center Staff Member
-        </p>
+        <p className="font-bold text-mediphix_accent">{role}</p>
         <p>at {medicalCenterName}</p>
       </div>
       <HiCheckBadge className="text-2xl text-mediphix_accent" />
--- src/pages/medical-center-receptionist/MCRHomePage.tsx
+++ src/pages/medical-center-receptionist/MCRHomePage.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { useState } from "react";
+import MCSNavBar from "../../components/mcs/MCSNavBar";
+import MCSMainGreeting from "../../components/mcs/MCSMainGreeting";
+import Loading from "../../components/Loading";
+import Footer from "../../components/Footer";
 
 function MCRHomePage() {
-  return <div>MCRHomePage</div>;
+  const breadcrumbItems = [
+    {
+      title: "Home",
+      link: "/medicalCenterReceptionist",
+    },
+  ];
+
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      {/* Navigation Bar  */}
+      <MCSNavBar />
+      {/* Body */}
+      {!loading && (
+        <div className="flex-grow px-8">
+          <MCSMainGreeting
+            title="Good Evening"
+            titleMemberName="Vishwa"
+            breadcrumbItems={breadcrumbItems}
+            role="Medical Center Receptionist"
+            medicalCenterName="Nawaloka Hospital"
+          />
+          {/* Main Body div */}
+        </div>
+      )}
+      {loading && <Loading />}
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
 }
 
 export default MCRHomePage;
